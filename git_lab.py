4244f63f00a598c7ba8cd4ede0c48e9555fd2700
marykatenussrallah <mnussrallah2@huskers.unl.edu>
2025-02-19 12:57:59 -0600

    Add multiply_numbers function and test

--- git_lab.py
+++ git_lab.py
@@ -68,3 +68,7 @@ def is_prime(n):
 def add_numbers(a, b):
     """Returns the sum of two numbers."""
     return a + b
+
+def multiply_numbers(a, b):
+    """Returns the product of two numbers."""
+    return a * b
